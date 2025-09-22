15231ee2c71e19fa119b907375199c256872e4ab
ssacredly-lab <ssacredly@gmail.com>
2025-09-22 22:24:03 +0100

    Update InputForm.tsx

--- InputForm.tsx
+++ InputForm.tsx
@@ -1,8 +1,8 @@
 
 
 import React from 'react';
-import type { FormState, OutfitImage } from '../types';
-import SectionHeader from './SectionHeader';
+import type { FormState, OutfitImage } from '../types.ts';
+import SectionHeader from './SectionHeader.tsx';
 
 interface InputFormProps {
   formState: FormState;
