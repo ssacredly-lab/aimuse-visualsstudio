e13d793233025e973425bc8d21f4cab2ef053a04
ssacredly-lab <ssacredly@gmail.com>
2025-09-22 22:26:09 +0100

    Update VideoOnlyInputForm.tsx

--- VideoOnlyInputForm.tsx
+++ VideoOnlyInputForm.tsx
@@ -1,8 +1,8 @@
 
 
 import React from 'react';
-import type { VideoOnlyFormState, OutfitImage } from '../types';
-import SectionHeader from './SectionHeader';
+import type { VideoOnlyFormState, OutfitImage } from '../types.ts';
+import SectionHeader from './SectionHeader.tsx';
 
 interface VideoOnlyInputFormProps {
   formState: VideoOnlyFormState;
